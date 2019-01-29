f6be936e06873120e55c8bf0c3ead6165d69e5e3
信昕 <xin@Xin-Xins-MacBook-Air.local>
2019-01-29 14:16:45 -0800

    assignment_04

---
+++ assignment_04/ex29.py
@@ -0,0 +1,35 @@
+people = 10
+cats = 30
+dogs = 25
+
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+
+dogs += 5
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:
+    print("People are dogs.")
+
+
+if 3 == 4:
+    print("OMG, 3 equals 4.")
+
+if 3 < 4:
+    print("3 is less than 4.")
